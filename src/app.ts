68eed8658bed2d0866553f61436496f854a1b54a
Joselu <joseluissgz97@gmail.com>
2024-05-11 12:50:00 +0200

    add: bookingsRouter

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import bookingsRouter from './routes/bookings.routes';
 import roomsRouter from './routes/rooms.routes';
 
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ const expresApp = express();
 expresApp.use(express.json());
 expresApp.use(express.text());
 expresApp.use(roomsRouter);
+expresApp.use(bookingsRouter);
 
 expresApp.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
