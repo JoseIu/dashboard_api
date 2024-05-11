fd4624b1c897ddbca965faacdba9af10f9507526
Joselu <joseluissgz97@gmail.com>
2024-05-11 12:36:31 +0200

    add: roomsRouter

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import roomsRouter from './routes/rooms.routes';
 
 const PORT = process.env.PORT || 3000;
 const expresApp = express();
 
 expresApp.use(express.json());
 expresApp.use(express.text());
+expresApp.use(roomsRouter);
 
 expresApp.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
