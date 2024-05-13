44d9a80dd38e1c1a1fb07f605d33e666a75b0eae
Joselu <joseluissgz97@gmail.com>
2024-05-13 17:38:47 +0200

    add: asyncRequest to getAllRooms and getRoomsById

--- src/controllers/roomController.ts
+++ src/controllers/roomController.ts
@@ -1,14 +1,30 @@
 import { Request, Response } from 'express';
 import roomList from '../data/roomList.json';
-export const getAllRooms = (req: Request, res: Response) => {
-  res.status(200).json(roomList);
+import { Room } from '../interfaces/room';
+import { asyncRequest } from '../services/getData.service';
+
+export const getAllRooms = async (req: Request, res: Response) => {
+  try {
+    const rooms = await asyncRequest<Room>({ data: roomList });
+
+    return res.status(200).json({ rooms });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
-export const getRoomsById = (req: Request, res: Response) => {
+export const getRoomsById = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const room = roomList.find(room => room.room.id === id);
 
-  if (!room) res.status(404).json({ message: `Room with id ${id} not found` });
+  const response = (await asyncRequest<Room>({ data: roomList })) as Room[];
 
-  res.status(200).json(room);
+  try {
+    const room = response.find(room => room.room.id === id);
+
+    if (!room) res.status(404).json({ message: `Room with id ${id} not found` });
+
+    return res.status(200).json(room);
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 };
