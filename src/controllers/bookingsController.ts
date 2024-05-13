84eda0b26b047a831646f05ac11b6f83fd0bff97
Joselu <joseluissgz97@gmail.com>
2024-05-13 17:38:02 +0200

    fix: adding "return" to getBookingById

--- src/controllers/bookingsController.ts
+++ src/controllers/bookingsController.ts
@@ -6,6 +6,7 @@ import { asyncRequest } from '../services/getData.service';
 export const getAllBookings = async (req: Request, res: Response) => {
   try {
     const bookingsList = await asyncRequest<Booking>({ data: bookingList as Booking[] });
+
     return res.status(200).json({ bookingsList });
   } catch (error) {
     return res.status(500).json({ message: 'Internal server error' });
@@ -14,12 +15,15 @@ export const getAllBookings = async (req: Request, res: Response) => {
 
 export const getBookingById = async (req: Request, res: Response) => {
   const { id } = req.params;
+
   const bookingsList = (await asyncRequest<Booking>({ data: bookingList as Booking[] })) as Booking[];
+
   try {
     const booking = bookingsList.find(booking => booking.guest.reservationID === id);
+
     if (!booking) return res.status(404).json({ message: `Booking with id ${id} not found` });
 
-    res.status(200).json(booking);
+    return res.status(200).json(booking);
   } catch (error) {
     return res.status(500).json({ message: 'Internal server error' });
   }
