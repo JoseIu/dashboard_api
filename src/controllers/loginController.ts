904980de1bf8010d062ee89dbec4491869923fe9
Joselu <joseluissgz97@gmail.com>
2024-05-13 12:02:18 +0200

    add: send token if existUser

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,12 +1,17 @@
+import 'dotenv/config';
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import authByEmailPwd from '../helpers/authByEmailPwd';
+
+const SECRET_KEY = process.env.SECRET_KEY;
 export const loginControlller = (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   try {
     const existUser = authByEmailPwd(email, password);
-    console.log(existUser);
-    return res.status(200).json(existUser);
+
+    const token = jwt.sign({ id: existUser.id, email: existUser.email }, SECRET_KEY!, { expiresIn: 86400 });
+    return res.status(200).json({ token });
   } catch (error) {
     res.status(401).json({ message: 'Incorrect credentials' });
   }
