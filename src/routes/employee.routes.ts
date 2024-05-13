6f99651da9bdcbdf8838de5f7398f256d32f4c00
Joselu <joseluissgz97@gmail.com>
2024-05-13 12:03:43 +0200

    add: middleware checkToken to eployee  routes

--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getAllEmployees, getEmployeeById } from '../controllers/employeeControoler';
+import checkToken from '../middleware/checkToken';
 
 const employeeRoutes = Router();
 
-employeeRoutes.get('/employees', getAllEmployees);
-employeeRoutes.get('/employee/:id', getEmployeeById);
+employeeRoutes.get('/employees', checkToken, getAllEmployees);
+employeeRoutes.get('/employee/:id', checkToken, getEmployeeById);
 
 export default employeeRoutes;
