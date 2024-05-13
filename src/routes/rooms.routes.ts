42af8c3aefacd3b5ace1c19ea3a07e06af37ecf7
Joselu <joseluissgz97@gmail.com>
2024-05-13 12:03:50 +0200

    add: middleware checkToken to rooms routes

--- src/routes/rooms.routes.ts
+++ src/routes/rooms.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { getAllRooms, getRoomsById } from '../controllers/roomController';
+import checkToken from '../middleware/checkToken';
 const roomsRouter = express.Router();
 
-roomsRouter.get('/rooms', getAllRooms);
-roomsRouter.get('/room/:id', getRoomsById);
+roomsRouter.get('/rooms', checkToken, getAllRooms);
+roomsRouter.get('/room/:id', checkToken, getRoomsById);
 
 export default roomsRouter;
