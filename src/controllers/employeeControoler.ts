7a1979d35ab2febcc943259e50d7c76996f85db1
Joselu <joseluissgz97@gmail.com>
2024-05-13 17:38:26 +0200

    add: asynRequest to getAllEmployees and getEmployeeById

--- src/controllers/employeeControoler.ts
+++ src/controllers/employeeControoler.ts
@@ -1,13 +1,30 @@
 import { Request, Response } from 'express';
 import employeeList from '../data/employeeList.json';
-export const getAllEmployees = (req: Request, res: Response) => {
-  res.status(200).json(employeeList);
+import { Employee } from '../interfaces/employee.interface';
+import { asyncRequest } from '../services/getData.service';
+
+export const getAllEmployees = async (req: Request, res: Response) => {
+  try {
+    const response = await asyncRequest<Employee>({ data: employeeList });
+
+    return res.status(200).json({ employeeList: response });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 };
-export const getEmployeeById = (req: Request, res: Response) => {
+
+export const getEmployeeById = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const employee = employeeList.find(({ employee }) => employee.employeeId === id);
 
-  if (!employee) return res.status(404).json({ message: 'Employee not found' });
+  const response = (await asyncRequest<Employee>({ data: employeeList })) as Employee[];
 
-  res.status(200).json(employee);
+  try {
+    const employee = response.find(employee => employee.employee.employeeId === id);
+
+    if (!employee) return res.status(404).json({ message: 'Employee not found' });
+
+    return res.status(200).json(employee);
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 };
