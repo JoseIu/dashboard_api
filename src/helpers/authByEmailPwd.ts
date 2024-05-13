6c80188ae0e2d371bc99e8ead0456f707ace3efd
Joselu <joseluissgz97@gmail.com>
2024-05-13 09:23:30 +0200

    fix: validating first email and after pwd

--- src/helpers/authByEmailPwd.ts
+++ src/helpers/authByEmailPwd.ts
@@ -2,9 +2,10 @@ import users from '../data/users.json';
 import { User } from '../interfaces/user.interface';
 
 const authByEmailPwd = (email: string, password: string): User => {
-  const existUser = users.find(user => user.email === email && user.password === password);
+  const existUser = users.find(user => user.email === email);
 
   if (!existUser) throw new Error();
+  if (existUser.password !== password) throw new Error();
 
   return existUser;
 };
