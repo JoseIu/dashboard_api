e195191a90f1cd2959e44dbc2732c7f8655b5b20
Joselu <joseluissgz97@gmail.com>
2024-05-14 12:46:39 +0200

    refactor: Refactor routes to use roomController methods

--- src/routes/rooms.routes.ts
+++ src/routes/rooms.routes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getAllRooms, getRoomById } from '../controllers/roomController';
+import roomController from '../controllers/roomController';
 import checkToken from '../middleware/checkToken';
 const roomsRouter = express.Router();
 
-roomsRouter.get('/rooms', checkToken, getAllRooms);
-roomsRouter.get('/room/:id', checkToken, getRoomById);
+roomsRouter.get('/rooms', checkToken, roomController.getAllRooms);
+roomsRouter.get('/room/:id', checkToken, roomController.getRoomById);
 
 export default roomsRouter;
