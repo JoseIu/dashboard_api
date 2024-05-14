783a5afca4ca35eaaddb2e47fc0ae462bb0067b4
Joselu <joseluissgz97@gmail.com>
2024-05-14 12:55:15 +0200

    refactor: Update employeeController to use responseCliente and catchedAsyc utility

--- src/controllers/employeeControoler.ts
+++ src/controllers/employeeControoler.ts
@@ -2,29 +2,28 @@ import { Request, Response } from 'express';
 import employeeList from '../data/employeeList.json';
 import { Employee } from '../interfaces/employee.interface';
 import { asyncRequest } from '../services/getData.service';
+import { catchedAsyc } from '../utils/catchedAsyc';
+import { ClientError } from '../utils/errorClient';
+import responseCliente from '../utils/responseCliente';
 
-export const getAllEmployees = async (req: Request, res: Response) => {
-  try {
-    const response = await asyncRequest<Employee>({ data: employeeList });
+const getAllEmployees = async (req: Request, res: Response) => {
+  const response = await asyncRequest<Employee>({ data: employeeList });
 
-    return res.status(200).json({ employeeList: response });
-  } catch (error) {
-    return res.status(500).json({ message: 'Internal server error' });
-  }
+  return responseCliente(res, 200, response);
 };
 
-export const getEmployeeById = async (req: Request, res: Response) => {
+const getEmployeeById = async (req: Request, res: Response) => {
   const { id } = req.params;
 
   const response = (await asyncRequest<Employee>({ data: employeeList })) as Employee[];
 
-  try {
-    const employee = response.find(employee => employee.employee.employeeId === id);
+  const employee = response.find(employee => employee.employee.employeeId === id);
+  if (!employee) throw new ClientError(`Employee with ${id} not found`, 404);
 
-    if (!employee) return res.status(404).json({ message: 'Employee not found' });
+  return responseCliente(res, 200, employee);
+};
 
-    return res.status(200).json(employee);
-  } catch (error) {
-    return res.status(500).json({ message: 'Internal server error' });
-  }
+export default {
+  getAllEmployees: catchedAsyc(getAllEmployees),
+  getEmployeeById: catchedAsyc(getEmployeeById)
 };
