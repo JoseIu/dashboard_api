78fc6c01241fe05fbc0a146261d3616ea329269f
Joselu <joseluissgz97@gmail.com>
2024-05-14 16:14:40 +0200

    refactor: Update server.ts to export server instance

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import expresApp from './app';
 
 const PORT = process.env.PORT || 3000;
 
-expresApp.listen(PORT, () => {
+const server = expresApp.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
 });
+
+export default server;
