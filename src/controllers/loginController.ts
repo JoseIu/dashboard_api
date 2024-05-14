17e918639d08bb914491c8686d4988b1e5cecdc1
Joselu <joseluissgz97@gmail.com>
2024-05-14 13:00:04 +0200

    refactor: Update loginController to use responseCliente and catchedAsyc utility

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -2,17 +2,25 @@ import 'dotenv/config';
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import authByEmailPwd from '../helpers/authByEmailPwd';
+import { catchedAsyc } from '../utils/catchedAsyc';
+import { ClientError } from '../utils/errorClient';
+import responseCliente from '../utils/responseCliente';
 
 const SECRET_KEY = process.env.SECRET_KEY;
-export const loginControlller = (req: Request, res: Response) => {
+
+const loginControlller = (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   try {
     const existUser = authByEmailPwd(email, password);
 
     const token = jwt.sign({ id: existUser.id, email: existUser.email }, SECRET_KEY!, { expiresIn: 86400 });
-    return res.status(200).json({ token });
+    return responseCliente(res, 200, { token });
   } catch (error) {
-    res.status(401).json({ message: 'Incorrect credentials' });
+    throw new ClientError('Invalid credentials', 401);
   }
+};
+
+export default {
+  loginControlller: catchedAsyc(loginControlller)
 };
