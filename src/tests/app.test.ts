9ace5f0bc0de2f05275fb9351a5b98709ba2f2bc
Joselu <joseluissgz97@gmail.com>
2024-05-14 14:50:35 +0200

    feat: Add employee API endpoints and tests and login

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -3,7 +3,9 @@ import request from 'supertest';
 import app from '../app';
 
 import bookings from '../data/bookings.json';
+import employeeList from '../data/employeeList.json';
 import roomList from '../data/roomList.json';
+import users from '../data/users.json';
 
 describe('Get /bookings', () => {
   const token = process.env.TOKEN_TEST;
@@ -35,7 +37,7 @@ describe('Get /bookings', () => {
       data: booking
     });
   });
-  test('Should response with a status 404 and ', async () => {
+  test('Should response with a status 404 if room not with ID not esist ', async () => {
     const response = await request(app).get('/booking/ABC1234').set('Authorization', `Bearer ${token}`);
 
     expect(response.status).toBe(404);
@@ -75,5 +77,75 @@ describe('Get /rooms', () => {
       error: false,
       data: room
     });
+  });
+});
+
+describe('GET /employees', () => {
+  const token = process.env.TOKEN_TEST;
+  const data = employeeList;
+
+  test('Should response with a 200 status code', async () => {
+    const response = await request(app).get('/employees').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+  });
+  test('Should response with a 401, status code if Unauthorized', async () => {
+    const response = await request(app).get('/employees');
+
+    expect(response.status).toBe(401);
+  });
+
+  test('Should response with all EMPLOYYE with a message {error:false: data:[]}', async () => {
+    const response = await request(app).get('/employees').set('Authorization', `Bearer ${token}`);
+
+    expect(response.body).toMatchObject({
+      error: false,
+      data
+    });
+  });
+
+  test('Should return with an employee searched by ID', async () => {
+    const response = await request(app).get('/employee/EMP001').set('Authorization', `Bearer ${token}`);
+    const employee = employeeList.find(employee => employee.employee.employeeId === 'EMP001');
+
+    expect(response.body).toMatchObject({
+      error: false,
+      data: employee
+    });
+  });
+  test('Should return with a 404 status code if employee with ID not exist', async () => {
+    const response = await request(app).get('/employee/EMP0012').set('Authorization', `Bearer ${token}`);
+    expect(response.status).toBe(404);
+  });
+});
+
+describe('POST /login', () => {
+  const usersList = users;
+  const userTes = usersList[0];
+  console.log(userTes);
+
+  test('should response a status code 200 if all is OK', async () => {
+    const response = await request(app).post('/login').send({ email: userTes.email, password: userTes.password });
+    expect(response.status).toBe(200);
+  });
+  test('Should response with a Token if credential are correct', async () => {
+    const response = await request(app).post('/login').send({ email: userTes.email, password: userTes.password });
+    expect(response.body).toMatchObject({
+      error: false,
+      data: {
+        token: expect.any(String)
+      }
+    });
+  });
+  test('Should response with a staus code 400 if some credential are missing', async () => {
+    const response = await request(app).post('/login').send({ email: userTes.email });
+
+    expect(response.status).toBe(400);
+  });
+  test('Should response with a error if some credential are missing', async () => {
+    const response = await request(app).post('/login').send({ email: userTes.email });
+    console.log(response.body);
+
+    expect(response.body).toEqual([expect.any(String)]);
   });
 });
