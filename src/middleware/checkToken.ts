74c8c70ff22e06657830aeaa57ce805b9a810200
Joselu <joseluissgz97@gmail.com>
2024-05-19 23:05:36 +0200

    refactor: Remove unused code in checkToken middleware

--- src/middleware/checkToken.ts
+++ src/middleware/checkToken.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import findUser from '../helpers/findUser';
 import { TokenPayload } from '../interfaces/tokenPayload.interface';
 
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -14,9 +13,10 @@ const checkToken = (req: Request, res: Response, next: NextFunction) => {
   const token = authorization.split(' ')[1];
   try {
     const decoded = jwt.verify(token, SECRET_KEY!) as TokenPayload;
-    const { email, id } = decoded;
-    const verifyUser = findUser(id, email);
-    if (verifyUser) return next();
+    // const { email, id } = decoded;
+    // const verifyUser = findUser(id, email);
+    // if (verifyUser)
+    return next();
   } catch (error) {
     return res.status(401).json({ message: 'User not found' });
   }
