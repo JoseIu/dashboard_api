76971b0c2a17746ef09a2fbe2b2183100585e537
Joselu <joseluissgz97@gmail.com>
2024-05-19 13:02:12 +0200

    refactor: Add database connection setup and Swagger configuration

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
+import conectarDB from './config/db';
 import bookingsRouter from './routes/bookings.routes';
 import employeeRoutes from './routes/employee.routes';
 import loginRoutes from './routes/login.routes';
 import roomsRouter from './routes/rooms.routes';
+import { setupSwagger } from './swagger.config';
 import { ClientError } from './utils/errorClient';
 
 const PORT = process.env.PORT || 3000;
 const expresApp = express();
+conectarDB();
 
 expresApp.use(express.json());
 expresApp.use(express.text());
@@ -16,6 +19,7 @@ expresApp.use(bookingsRouter);
 expresApp.use(employeeRoutes);
 expresApp.use(loginRoutes);
 
+setupSwagger(expresApp);
 expresApp.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if ('statusCode' in err) {
     res.status((err as ClientError).statusCode).json({
