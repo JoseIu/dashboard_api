171162e4ddc70728a3a9759684c9b55c61fd60be
Joselu <joseluissgz97@gmail.com>
2024-05-20 21:46:57 +0200

    refactor: Update bookingsController to use BookingInterface for type checking

--- src/controllers/bookingsController.ts
+++ src/controllers/bookingsController.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { BookingInterface } from '../interfaces/booking.inerface';
 import Booking from '../models/Booking';
 import { catchedAsyc } from '../utils/catchedAsyc';
 import { ClientError } from '../utils/errorClient';
 import responseCliente from '../utils/responseCliente';
 
 const getAllBookings = async (req: Request, res: Response) => {
-  const bookingsList = await Booking.find();
+  const bookingsList: BookingInterface[] = await Booking.find();
 
   return responseCliente(res, 200, bookingsList);
 };
 const getBookingById = async (req: Request, res: Response) => {
   const { id } = req.params;
   if (!id) throw new ClientError('Id is required', 400);
+  if (!mongoose.Types.ObjectId.isValid(id)) throw new ClientError(`Invalid id: ${id}`, 400);
 
   const booking = await Booking.findOne({ _id: id });
+
+  console.log(booking);
 
   if (!booking) throw new ClientError(`Booking with id ${id} not found`, 404);
 
