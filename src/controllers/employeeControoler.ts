7b4f3c94ccf129e1b86ce57f7d2cd9677182c36a
Joselu <joseluissgz97@gmail.com>
2024-05-20 16:22:13 +0200

    refactor: Update getAllEmployees controller to use MongoDB model for employees

--- src/controllers/employeeControoler.ts
+++ src/controllers/employeeControoler.ts
@@ -1,23 +1,22 @@
 import { Request, Response } from 'express';
-import employeeList from '../data/employeeList.json';
-import { Employee } from '../interfaces/employee.interface';
-import { asyncRequest } from '../services/getData.service';
+import Employee from '../models/Employee';
 import { catchedAsyc } from '../utils/catchedAsyc';
 import { ClientError } from '../utils/errorClient';
 import responseCliente from '../utils/responseCliente';
 
 const getAllEmployees = async (req: Request, res: Response) => {
-  const response = await asyncRequest<Employee>({ data: employeeList });
+  const employeeList = await Employee.find();
 
-  return responseCliente(res, 200, response);
+  return responseCliente(res, 200, employeeList);
 };
 
 const getEmployeeById = async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  const response = (await asyncRequest<Employee>({ data: employeeList })) as Employee[];
+  if (!id) throw new ClientError('Id is required', 400);
 
-  const employee = response.find(employee => employee.employee.employeeId === id);
+  const employee = await Employee.findOne({ 'employee.employeeId': id });
+
   if (!employee) throw new ClientError(`Employee with ${id} not found`, 404);
 
   return responseCliente(res, 200, employee);
