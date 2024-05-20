b47a926a7cc993cbb79118b5c298b571bbe2082e
Joselu <joseluissgz97@gmail.com>
2024-05-20 12:31:59 +0200

    feat: Seed employees collection with dummy data

---
+++ src/config/seedEmployees.ts
@@ -0,0 +1,47 @@
+import { faker } from '@faker-js/faker';
+import 'dotenv/config';
+import { MongoClient } from 'mongodb';
+import { Employee } from '../interfaces/employee.interface';
+const uri = process.env.MONGO_URI;
+const seedDB = async () => {
+  const client = new MongoClient(uri!);
+
+  try {
+    await client.connect();
+    console.log('Connected correctly to server');
+
+    const collection = client.db('dashboard_api').collection('employees');
+
+    await collection.drop();
+
+    let employeesList: Employee[] = [];
+
+    for (let i = 0; i < 20; i++) {
+      let employee: Employee = {
+        employee: {
+          image: faker.image.avatar(),
+          firstName: faker.person.firstName(),
+          lastName: faker.person.lastName(),
+          employeeId: `E${faker.number.int({ min: 1, max: 100 })}`,
+          startDate: faker.date.anytime().toISOString().slice(0, 10)
+        },
+        contact: {
+          phone: faker.phone.number(),
+          email: faker.internet.email()
+        },
+        description: faker.lorem.sentence({ min: 5, max: 10 }),
+        status: faker.datatype.boolean()
+      };
+
+      employeesList.push(employee);
+    }
+    await collection.insertMany(employeesList);
+
+    console.log('Database seeded! :)');
+    await client.close();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+seedDB();
