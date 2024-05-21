98777efe2329c6b170e7ea45721b0a5bac3b5b8f
Joselu <joseluissgz97@gmail.com>
2024-05-21 21:10:23 +0200

    feat: Add CRUD endpoints for employees

--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -7,5 +7,8 @@ const employeeRoutes = Router();
 
 employeeRoutes.get('/employees', checkToken, employeeControoler.getAllEmployees);
 employeeRoutes.get('/employee/:id?', checkToken, employeeControoler.getEmployeeById);
+employeeRoutes.post('/employee', checkToken, employeeControoler.createNewEmployee);
+employeeRoutes.put('/employee/:id', checkToken, employeeControoler.updateEmployee);
+employeeRoutes.delete('/employee/:id', checkToken, employeeControoler.deleteEmployee);
 
 export default employeeRoutes;
