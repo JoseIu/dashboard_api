472945d854f0439b588e99edbef4cd6f27047b37
Joselu <joseluissgz97@gmail.com>
2024-05-21 14:26:13 +0200

    feat: Add endpoint for creating new bookings

--- src/routes/bookings.routes.ts
+++ src/routes/bookings.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import bookingController from '../controllers/bookingsController';
+import createNewBookingDTO from '../dto/createNewBooking.dto';
 import checkToken from '../middleware/checkToken';
 
 const bookingsRouter = Router();
 
 bookingsRouter.get('/bookings', checkToken, bookingController.getAllBookings);
 bookingsRouter.get('/booking/:id?', checkToken, bookingController.getBookingById);
+
+bookingsRouter.post('/booking/add-booking', checkToken, createNewBookingDTO, bookingController.createNewBooking);
 
 export default bookingsRouter;
