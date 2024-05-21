d93d0250b19ab614dcac5ca4f3cdc991e8fbf365
Joselu <joseluissgz97@gmail.com>
2024-05-21 21:10:12 +0200

    fix: fixing "__v"  with "versionKey: false"

--- src/models/Booking.ts
+++ src/models/Booking.ts
@@ -1,28 +1,31 @@
 import mongoose from 'mongoose';
 
-const bookingSchema = new mongoose.Schema({
-  orderDate: {
-    type: Date,
-    required: true
+const bookingSchema = new mongoose.Schema(
+  {
+    orderDate: {
+      type: Date,
+      required: true
+    },
+    checkin: {
+      date: { type: Date, required: true },
+      time: { type: String, required: true }
+    },
+    checkOut: {
+      date: { type: Date, required: true },
+      time: { type: String, required: true }
+    },
+    specialRequest: String,
+    roomType: String,
+    status: String,
+    guest: {
+      name: { type: String, required: true },
+      lastName: { type: String, required: true },
+      reserVationId: { type: String, required: true },
+      img: { type: String, required: true }
+    }
   },
-  checkin: {
-    date: { type: Date, required: true },
-    time: { type: String, required: true }
-  },
-  checkOut: {
-    date: { type: Date, required: true },
-    time: { type: String, required: true }
-  },
-  specialRequest: String,
-  roomType: String,
-  status: String,
-  guest: {
-    name: { type: String, required: true },
-    lastName: { type: String, required: true },
-    reserVationId: { type: String, required: true },
-    img: { type: String, required: true }
-  }
-});
+  { versionKey: false }
+);
 
 const Booking = mongoose.model('Booking', bookingSchema);
 
