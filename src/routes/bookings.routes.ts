fc6da075899642c15065b6e706459fc791ca5b2d
Joselu <joseluissgz97@gmail.com>
2024-05-22 17:38:12 +0200

    refactor: Update booking routes to follow RESTful conventions

--- src/routes/bookings.routes.ts
+++ src/routes/bookings.routes.ts
@@ -9,9 +9,9 @@ const bookingsRouter = Router();
 bookingsRouter.get('/bookings', checkToken, bookingController.getAllBookings);
 bookingsRouter.get('/booking/:id?', checkToken, bookingController.getBookingById);
 
-bookingsRouter.post('/booking/add-booking', checkToken, createNewBookingDTO, bookingController.createNewBooking);
+bookingsRouter.post('/booking', checkToken, createNewBookingDTO, bookingController.createNewBooking);
 
-bookingsRouter.put('/booking/update-booking/:id?', checkToken, bookingController.updateBooking);
-bookingsRouter.delete('/booking/delete-booking/:id?', checkToken, bookingController.deleteBooking);
+bookingsRouter.put('/booking/:id?', checkToken, bookingController.updateBooking);
+bookingsRouter.delete('/booking/:id?', checkToken, bookingController.deleteBooking);
 
 export default bookingsRouter;
