db69813105c27fb57ff24afcd3d3695f241805fd
Joselu <joseluissgz97@gmail.com>
2024-05-23 14:56:01 +0200

    refactor: Update User model schema to remove versionKey field

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+  },
+  { versionKey: false }
+);
 
 const User = mongoose.model('User', userSchema);
 export default User;
