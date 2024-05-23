1a23a5f9345372a67f8aa0121410a4bfda50cbc6
Joselu <joseluissgz97@gmail.com>
2024-05-23 18:13:05 +0200

    feat: Add password encryption to User model and methods.checkPWD

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,13 @@
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  checkPWD(passwordForm: string): Promise<boolean>;
+}
 
-const userSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema<IUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
@@ -8,6 +15,14 @@ const userSchema = new mongoose.Schema(
   },
   { versionKey: false }
 );
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
+userSchema.methods.checkPWD = async function (passwordForm: string) {
+  return await bcrypt.compare(passwordForm, this.password);
+};
 const User = mongoose.model('User', userSchema);
 export default User;
