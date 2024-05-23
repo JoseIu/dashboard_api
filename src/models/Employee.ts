d97a97fb88067f3aa82c061b05bb23ca50df464b
Joselu <joseluissgz97@gmail.com>
2024-05-23 14:55:49 +0200

    refactor: Add description and contact fields to Employee model

--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -1,20 +1,23 @@
 import mongoose from 'mongoose';
 
-const employeeSchema = new mongoose.Schema({
-  employee: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    employeeId: { type: String, required: true },
-    startDate: { type: Date, required: true },
-    image: { type: String, required: true }
+const employeeSchema = new mongoose.Schema(
+  {
+    employee: {
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
+      employeeId: { type: String, required: true },
+      startDate: { type: Date, required: true },
+      image: { type: String, required: true }
+    },
+    description: { type: String, required: true },
+    contact: {
+      phone: { type: String, required: true },
+      email: { type: String, required: true }
+    },
+    status: { type: Boolean, required: true }
   },
-  description: { type: String, required: true },
-  contact: {
-    phone: { type: String, required: true },
-    email: { type: String, required: true }
-  },
-  status: { type: Boolean, required: true }
-});
+  { versionKey: false }
+);
 
 const Employee = mongoose.model('Employee', employeeSchema);
 
