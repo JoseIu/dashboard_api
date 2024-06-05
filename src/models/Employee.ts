ff728ca3cf9513901849eaf3432b4c03f91c03f2
Joselu <joseluissgz97@gmail.com>
2024-06-05 09:27:06 +0200

    refactor: Update Employee model to include password hashing in pre-save hook

--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -1,24 +1,28 @@
 import mongoose from 'mongoose';
-
+const bcrypt = require('bcryptjs');
 const employeeSchema = new mongoose.Schema(
   {
-    employee: {
-      firstName: { type: String, required: true },
-      lastName: { type: String, required: true },
-      employeeId: { type: String, required: true },
-      startDate: { type: Date, required: true },
-      image: { type: String, required: true }
-    },
+    image: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+    startDate: { type: Date, required: true },
+
     description: { type: String, required: true },
-    contact: {
-      phone: { type: String, required: true },
-      email: { type: String, required: true }
-    },
+
+    phone: { type: String, required: true },
+    email: { type: String, required: true },
+
     status: { type: Boolean, required: true }
   },
   { versionKey: false }
 );
-
+employeeSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 const Employee = mongoose.model('Employee', employeeSchema);
 
 export default Employee;
