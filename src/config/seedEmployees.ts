84d3773a75025a5f95f9a748635b9cebae44b815
Joselu <joseluissgz97@gmail.com>
2024-06-05 09:27:15 +0200

    refactor: Update seed script to include password hashing in seedEmployees.ts

--- src/config/seedEmployees.ts
+++ src/config/seedEmployees.ts
@@ -2,6 +2,8 @@ import { faker } from '@faker-js/faker';
 import 'dotenv/config';
 import { MongoClient } from 'mongodb';
 import { EmployeeInterface } from '../interfaces/employee.interface';
+const bcrypt = require('bcryptjs');
+
 const uri = process.env.MONGO_URI;
 const seedDB = async () => {
   const client = new MongoClient(uri!);
@@ -18,17 +20,14 @@ const seedDB = async () => {
 
     for (let i = 0; i < 20; i++) {
       let employee: EmployeeInterface = {
-        employee: {
-          image: faker.image.avatar(),
-          firstName: faker.person.firstName(),
-          lastName: faker.person.lastName(),
-          employeeId: `E${faker.number.int({ min: 1, max: 100 })}`,
-          startDate: faker.date.anytime().toISOString().slice(0, 10)
-        },
-        contact: {
-          phone: faker.phone.number(),
-          email: faker.internet.email()
-        },
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        password: bcrypt.hashSync('prueba', 10),
+        image: faker.image.avatar(),
+        startDate: faker.date.anytime().toISOString().slice(0, 10),
+        phone: faker.phone.number(),
+        email: faker.internet.email(),
+        role: faker.helpers.arrayElement(['Manager', 'Receptionist', 'Room Services']),
         description: faker.lorem.sentence({ min: 5, max: 10 }),
         status: faker.datatype.boolean()
       };
