07ac70c90505c4befd48a3d04845b3d2d547ee12
Joselu <joseluissgz97@gmail.com>
2024-06-05 09:28:10 +0200

    refactor: Update seed script to use seedEmployees.ts for employee data

--- src/routes/rooms.routes.ts
+++ src/routes/rooms.routes.ts
@@ -6,7 +6,7 @@ const roomsRouter = express.Router();
 roomsRouter.get('/rooms', checkToken, roomController.getAllRooms);
 roomsRouter.get('/room/:id?', checkToken, roomController.getRoomById);
 roomsRouter.post('/room', checkToken, roomController.createNewRoom);
-roomsRouter.put('/room/:id', checkToken, roomController.updateRoom);
+roomsRouter.put('/room/:id?', checkToken, roomController.updateRoom);
 roomsRouter.delete('/room/:id', checkToken, roomController.deleteRoom);
 
 export default roomsRouter;
