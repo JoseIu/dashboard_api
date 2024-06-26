ae556b36fde2b974fa7d3011a70826ef69a27481
Joselu <joseluissgz97@gmail.com>
2024-06-26 23:05:30 +0200

    refactor: Update deleteContact function to handle deleting a contact by ID

--- src/controllers/contactsController.ts
+++ src/controllers/contactsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Contact from '../models/Contact';
 import { catchedAsyc } from '../utils/catchedAsyc';
+import { ClientError } from '../utils/errorClient';
 import responseCliente from '../utils/responseCliente';
 
 const getAllContacts = async (req: Request, res: Response) => {
@@ -13,7 +14,14 @@ const getContactById = async (req: Request, res: Response) => {};
 
 const createNewContact = async (req: Request, res: Response) => {};
 const updateContact = async (req: Request, res: Response) => {};
-const deleteContact = async (req: Request, res: Response) => {};
+const deleteContact = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const contact = await Contact.findById(id);
+  if (!contact) throw new ClientError(`Booking with id ${id} not found`, 404);
+
+  await contact.deleteOne();
+  return responseCliente(res, 200, 'Booking deleted');
+};
 
 export default {
   getAllContacts: catchedAsyc(getAllContacts),
